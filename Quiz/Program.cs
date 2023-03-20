276967e5a0969f7bc9e3c44cfa2350637366a9ab
LordPelmeha <sashasofolov@gmail.com>
2023-03-20 11:04:41 +0300

    Added SplitFileByString

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Quiz
+﻿using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+
+namespace Quiz
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+            var s = "bank.txt";
+        }
+        static string[] SplitFileByString(string path)
+        {
+            using (var rs = new StreamReader(File.Open(path, FileMode.Open)))
+            { 
+                var s = Regex.Split(rs.ReadToEnd(), @"\n").Select(x => x.ToString()).ToArray();
+                return s[..(s.Length-1)];
+            }
         }
     }
-}
+} 
