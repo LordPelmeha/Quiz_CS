b0913b4ed10f0799cdef869c0b85ff159745c309
LordPelmeha <sashasofolov@gmail.com>
2023-03-20 20:35:34 +0300

    Added several event handlers

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -23,12 +23,17 @@ namespace Quiz
             Regex.Matches(s, @"(?<=\|\|).*?(?=\=)").Select(x => x.ToString()).ToArray();
         static int RightAnswer(string s) =>
             Regex.Matches(s, @"(?<=\=\>).*?(?=[\|\n])").Select(x => x.ToString()).ToList().FindIndex(x => x == "True") + 1;
-        static int GetPlayerAnswer()
+        static int GetPlayerAnswer(int count)
         {
             var ans = Console.ReadLine();
             while (ans.Length > 1 || !"123345".Contains(ans) || ans == "")
             {
                 Console.WriteLine("Вы ввели некорректный ответ! Пожалуйста, введи ответ правильно.");
+                ans = Console.ReadLine();
+            }
+            while (count == 0 && int.Parse(ans) == 5)
+            {
+                Console.WriteLine("Вы использовали все помощи! Введите ответ на вопрос.");
                 ans = Console.ReadLine();
             }
             return int.Parse(ans);
@@ -43,11 +48,11 @@ namespace Quiz
                 Console.WriteLine($"{num}) {ans[num - 1]}   {chek}) {ans[chek - 1]}");
             else
                 Console.WriteLine($"{chek}) {ans[chek - 1]}   {num}) {ans[num - 1]}");
-            var a = GetPlayerAnswer();
+            var a = GetPlayerAnswer(2);
             while (a != num && a != chek && a == 5)
             {
                 Console.WriteLine("Вы уже использовали помощь! Введите ответ из предложенных вариантов.");
-                a = GetPlayerAnswer();
+                a = GetPlayerAnswer(2);
             }
             return a;
         }
@@ -59,6 +64,7 @@ namespace Quiz
             int chek;
             int score = 0;
             int playerAns;
+            int dopomogaCount = 2;
             for (int i = 0; i < str.Length; i++)
             {
                 Console.WriteLine($"Вопрос №{i + 1}!");
@@ -69,10 +75,29 @@ namespace Quiz
                     Console.Write($"{j + 1}) {answer[j]}   ");
                 Console.WriteLine();
                 Console.WriteLine("Ваш ответ?");
-                playerAns = GetPlayerAnswer();
+                playerAns = GetPlayerAnswer(dopomogaCount);
                 if (playerAns == 5)
+                {
                     playerAns = Dopomoga(answer, chek);
-                Console.WriteLine(playerAns);
+                    if (playerAns == chek)
+                    {
+                        dopomogaCount--;
+                        score += 5;
+                        Console.WriteLine("Верный ответ дали вы");
+                    }
+                    else
+                        Console.WriteLine("Ну ответили неправильно и ответили. Чего бубнеть то?");
+                }
+                else
+                {
+                    if (playerAns == chek)
+                    {
+                        score += 10;
+                        Console.WriteLine("Верный ответ дали вы");
+                    }
+                    else
+                        Console.WriteLine("Ну ответили неправильно и ответили. Чего бубнеть то?");
+                }
             }
         }
     }
